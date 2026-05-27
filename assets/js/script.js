b5c49a814219cfe9b8bfa00e8aac9bb9ae68a8a6
semegen <iurii.semegen@webmil.eu>
2026-05-27 14:14:02 +0300

    mobile navbar development

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log('JS work')
+const iconBlock = document.getElementById('iconBlock')
+const iconOpen= document.getElementById('iconOpen')
+const iconClose= document.getElementById('iconClose')
+const mainMenu= document.getElementById('mainMenu')
+
+iconBlock.addEventListener('click', () => {
+    iconOpen.classList.toggle('d-none')
+    iconClose.classList.toggle('d-none')
+    mainMenu.classList.toggle('menu-open')
+})
